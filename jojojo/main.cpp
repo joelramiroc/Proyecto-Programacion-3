7a2756b917a548ee94e1b7d429d5582a1abf2e33
Joel Caballero <www.joelramiro@unitec.edu>
2017-08-22 15:33:05 -0600

    dos figuras dibujadas.. se uso polimorfismo herencia.

--- jojojo/main.cpp
+++ jojojo/main.cpp
@@ -1,15 +1,22 @@
 #include<SFML/Graphics.hpp>
 #include "Game.h"
+#include "Window.h"
 #include <memory>
 #include<iostream>
 using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1200, 650), "Rubik's Cube!");
-    //tipo vector... para eliminar automaticamente el espacio de memoria reservado por punteros usando el include memory
-    shared_ptr<Game> miJuego(new Game(&window));
+    const auto &title = "Rubik's Cube!";
+    sf::VideoMode video_mode(1200, 650);
+    sf::RenderWindow renderWindow(video_mode, title);
+    Window window(renderWindow);
+    Game game(window);
+    game.run();
 
-    miJuego->run();
+
+
+
+
     return 0;
 }
