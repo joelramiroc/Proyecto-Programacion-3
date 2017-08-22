5556754355e9dd5a74073cf2c5dd78ea632cc4e7
Joel Caballero <www.joelramiro@unitec.edu>
2017-08-22 11:14:06 -0600

    dibujada primer forma

--- jojojo/main.cpp
+++ jojojo/main.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(1200, 650), "Rubik's Cube!");
     //tipo vector... para eliminar automaticamente el espacio de memoria reservado por punteros usando el include memory
-    shared_ptr<Game> miJuego(new Game());
+    shared_ptr<Game> miJuego(new Game(&window));
 
     miJuego->run();
     return 0;
